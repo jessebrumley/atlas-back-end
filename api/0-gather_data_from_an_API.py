db9ed20b52dd1e9778094c124a2c97753ae0318d
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-16 22:03:23 -0500

    FINALLY

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-""" Script using REST API """
+"""Script using REST API to fetch user name and todo list."""
 import requests
 import sys
+import json
+
+
+def get_name(employee_id):
+    """Fetches the name of the employee by ID."""
+    url = "https://jsonplaceholder.typicode.com/users/"
+    response = requests.get(url, params={'id': employee_id})
+    if response.status_code == 200:
+        users = response.json()
+        for user in users:
+            if user['id'] == employee_id:
+                return user.get('name', 'No name found')
+    return ('Failed to fetch name')
+
+
+def get_todos(employee_id):
+    """Fetches list of employees by ID."""
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    response = requests.get(url, params={'userId': employee_id})
+    if response.status_code == 200:
+        todo_list = response.json()
+        completed_tasks = []
+        for task in todo_list:
+            if task["completed"]:
+                completed_tasks.append(task["title"])
+        total_done = len(completed_tasks)
+        total_todo = len(todo_list)
+        
+        return (total_done, total_todo, completed_tasks)
+    return []
 
 
 def get_employee_todo(employee_id):
-    """
-    Retrieves and displays the TODO list of an employee using a REST API.
-
-    Parameters:
-    - employee_id: int
-        The ID of the employee for whom the TODO list is to be retrieved.
-    """
-
-    # Setting API paths
-    url = "https://jsonplaceholder.typicode.com/"
-    user_ext = f"{url}/users/{employee_id}]"
-    todo_ext = f"{url}/todos"
-
-    # Sending a GET request to the API endpoint
-    employee_info = requests.get(user_ext).json()
-    employee_name = employee_info.get('name')
-    todo_list = requests.get(f"{todo_ext}?userId={user_ext}").json()
-
-    # Counting the number of completed tasks
-    completed_tasks = []
-    for task in todo_list:
-        if task["completed"]:
-            completed_tasks.append(task["title"])
-
-    # Counting the totals of tasks and tasks done
-    total_todo = len(todo_list)
-    total_done = len(completed_tasks)
-
-    # Displaying the employee TODO list progress
+    """Fetches a user's total, completed, and titles of a todo list by ID."""
+    name = get_name(employee_id)
+    todos = get_todos(employee_id)
     print(
-        f"Employee {employee_name} is done with tasks("
-        f"{total_done}/{total_todo}):"
+        f"Employee {name} is done with tasks("
+        f"{todos[0]}/{todos[1]}):"
         )
-
-    # Displaying the titles of completed tasks
+    completed_tasks = todos[2]
     for completed_task in completed_tasks:
         print(f"\t {completed_task}")
 
 
 if __name__ == "__main__":
-    pass
+    employee_id = int(sys.argv[1])
+    get_employee_todo(employee_id)
