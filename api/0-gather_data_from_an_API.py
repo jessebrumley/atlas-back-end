cfd062db2f62c54519d4812b0d5da8f31001ac9f
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-16 22:11:56 -0500

    test 4

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -48,6 +48,5 @@ def get_employee_todo(employee_id):
 
 
 if __name__ == "__main__":
-    # employee_id = int(sys.argv[1])
-    # get_employee_todo(employee_id)
-    pass
+    employee_id = int(sys.argv[1])
+    get_employee_todo(employee_id)
