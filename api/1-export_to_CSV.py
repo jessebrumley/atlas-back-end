fb25399ecfd605589f4186849e5fa3f7fd055466
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-18 19:18:40 -0500

    imports alphabetical

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 """Script using REST API to fetch user name and todo list and export to CSV."""
+import csv
 import requests
 import sys
-import csv
 
 
 def get_name(employee_id):
