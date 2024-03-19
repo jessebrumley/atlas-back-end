b9a58c361f4c66ad964c6806ab3ca7312cd32a28
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-18 21:37:08 -0500

    3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,54 @@
+import requests
+import json
+
+
+def get_name(employee_id):
+    url = "https://jsonplaceholder.typicode.com/users/"
+    response = requests.get(url, params={'id': employee_id})
+    if response.status_code == 200:
+        users = response.json()
+        for user in users:
+            if user['id'] == employee_id:
+                return user.get('name', 'No name found')
+    return ('Failed to fetch name')
+
+
+def get_todos(employee_id):
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    response = requests.get(url, params={'userId': employee_id})
+    if response.status_code == 200:
+        todo_list = response.json()
+        tasks = []
+        for task in todo_list:
+            tasks.append({
+                "username": get_name(employee_id),
+                "task": task["title"],
+                "completed": task["completed"]
+            })
+        return tasks
+    return []
+
+
+def get_all_employees_todos():
+    url = "https://jsonplaceholder.typicode.com/users/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        users = response.json()
+        all_tasks = {}
+        for user in users:
+            user_id = str(user['id'])
+            tasks = get_todos(user['id'])
+            if tasks:
+                all_tasks[user_id] = tasks
+        return all_tasks
+    return {}
+
+
+def export_all_employees_todos():
+    all_tasks = get_all_employees_todos()
+    with open("todo_all_employees.json", "w") as outfile:
+        json.dump(all_tasks, outfile, indent=None)
+
+
+if __name__ == "__main__":
+    export_all_employees_todos()
